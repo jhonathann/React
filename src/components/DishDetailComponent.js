1a035160022c319ccd6baf862d75d2ec48c795b9
Jhonathann A. Gómez V <jhagomezve@unal.edu.co>
2022-04-07 18:02:06 -0500

    Assignment 3

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardImg,
@@ -7,9 +7,110 @@ import {
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Row,
+  Col,
+  Label,
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import { Control, LocalForm, Errors } from "react-redux-form";
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+function CommentForm() {
+  const [isModalOpen, setModal] = useState(false);
+  const toggleModal = () => setModal(!isModalOpen);
+  const handleSubmit = (values) => {
+    console.log("Current State is: " + JSON.stringify(values));
+    alert("Current State is: " + JSON.stringify(values));
+  };
+  return (
+    <div>
+      <Button outline onClick={toggleModal}>
+        <span className="fa fa-pencil "> Submit Comment</span>
+      </Button>
+      <Modal isOpen={isModalOpen} toggle={toggleModal}>
+        <ModalHeader>
+          <b>Submit Comment</b>
+        </ModalHeader>
+        <ModalBody>
+          <LocalForm onSubmit={(values) => handleSubmit(values)}>
+            <Row className="form-group">
+              <Col>
+                <Label htmlFor="rating">
+                  <b>Rating </b>
+                </Label>
+                <Control.select
+                  model=".rating"
+                  name="rating"
+                  className="form-control"
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Control.select>
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Col>
+                <Label htmlFor="author">
+                  <b>Your Name </b>
+                </Label>
+                <Control.text
+                  model=".author"
+                  id="author"
+                  name="author"
+                  placeholder="Your Name"
+                  className="form-control"
+                  validators={{
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15),
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".author"
+                  show="touched"
+                  messages={{
+                    required: " Required",
+                    minLength: " Must be greater than 2 characters",
+                    maxLength: " Must be 15 characters or less",
+                  }}
+                />
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Col>
+                <Label htmlFor="comment">Comment</Label>
+                <Control.textarea
+                  model=".comment"
+                  id="comment"
+                  name="comment"
+                  rows="6"
+                  className="form-control"
+                />
+              </Col>
+            </Row>
+            <Row className="form-group">
+              <Col>
+                <Button type="submit" color="primary">
+                  Submit
+                </Button>
+              </Col>
+            </Row>
+          </LocalForm>
+        </ModalBody>
+      </Modal>
+    </div>
+  );
+}
 function RenderDish({ dish }) {
   if (dish != null) {
     return (
@@ -42,7 +143,12 @@ function RenderComments({ comments }) {
         </li>
       );
     });
-    return commentary;
+    return (
+      <div>
+        {commentary}
+        <CommentForm></CommentForm>
+      </div>
+    );
   } else {
     return <div></div>;
   }
