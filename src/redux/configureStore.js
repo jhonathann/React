cb5c2cdf70cff8e5812c1e3434165bb13a6c3ca9
Jhonathann A. Gómez V <jhagomezve@unal.edu.co>
2022-04-11 18:12:03 -0500

    React Redux Form Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createForms } from "react-redux-form";
 import { Dishes } from "./dishes.js";
 import { Comments } from "./comments.js";
 import { Promotions } from "./promotions.js";
 import { Leaders } from "./leaders.js";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+import { InitialFeedback } from "./forms.js";
 export const ConfigureStore = () => {
   const store = createStore(
     combineReducers({
@@ -12,6 +14,7 @@ export const ConfigureStore = () => {
       comments: Comments,
       promotions: Promotions,
       leaders: Leaders,
+      ...createForms({ feedback: InitialFeedback }),
     }),
     applyMiddleware(thunk, logger)
   );
